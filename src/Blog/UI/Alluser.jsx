277efe89cc809e8b55725e8151e2823cc0d65265
mitenAspire <mitenkumar.patel@aspiresoftserv.in>
2023-03-13 23:42:00 +0530

    "change role comlete "

--- src/Blog/UI/Alluser.jsx
+++ src/Blog/UI/Alluser.jsx
@@ -1,100 +1,84 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
-import {
-  MDBBtn,
-  MDBModal,
-  MDBModalDialog,
-  MDBModalContent,
-  MDBModalHeader,
-  MDBModalTitle,
-  MDBModalBody,
-  MDBModalFooter,
-} from "mdb-react-ui-kit";
-const EmpListing = (props) => {
-  // const navigate = useNavigate();
-  const [empdata, empdatachange] = useState(null);
-  const [basicModal, setBasicModal] = useState(false);
-
-  const toggleShow = () => setBasicModal(!basicModal);
-  //   const LoadEdit = (id) => {
-  //     navigate("/employee/edit/" + id);
-  // }
+const Users = () => {
+  const [state, setState] = useState([]);
+  const[reloade,setreload]=useState(true)
+  const ChangeRole = (props, id) => {
+    if (props.role === "admin") {
+      axios
+        .put("http://localhost:8000/User/" + id, {
+          username: props.username,
+          password:props.password,
+          email: props.email,
+          role: "user",
+        })
+        .then((response) => {
+          console.log(response);
+          console.log("Changed to User");
+          setreload(!reloade)
+        });
+    } else {
+      axios
+        .put("http://localhost:8000/User/" + id, {
+          username: props.username,
+          password:props.password,
+          email: props.email,
+          role: "admin",
+        })
+        .then((response) => {
+          
+          // console.log(response['data']);
+          console.log(response);
+          console.log("Changed to Admin");
+          setreload(!reloade)
+        });
+    }
+  }
   useEffect(() => {
-    fetch("http://localhost:8000/User")
-      .then((res) => {
-        return res.json();
-      })
-      .then((resp) => {
-        empdatachange(resp);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+    axios.get("http://localhost:8000/User").then((response) => {
+      // console.log(response['data']);
+      setState([...response["data"]]);
+    });
+  }, [reloade]);
+
   return (
-    <div className="container my-3  border ">
-      <div className="card">
-        {/* aDD BUTTON................... */}
+    <div className="container p-0 mt-2">
+      <table className="table table-hover table-primary text-center">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First Name</th>
 
-        <div className="card-body ">
-          <table className="table table-bordered  ">
-            <thead className="bg-dark text-white ">
-              <tr>
-                <td>ID</td>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Role</td>
-                <td>Action</td>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((elem) => {
+            return (
+              <tr key={elem.id} className="p-0">
+                <th scope="col">{elem.id}</th>
+                <th scope="col">{elem.username}</th>
+
+                <th scope="col">{elem.email}</th>
+                <th scope="col">{elem.role}</th>
+                <th scope="col">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => ChangeRole(elem, elem.id)}
+                  >
+                    Change Role
+                  </button>
+                </th>
               </tr>
-            </thead>
-            <tbody className="table-primary">
-              {empdata &&
-                empdata.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.username}</td>
-                    <td>{item.email}</td>
-                    <td>{item.role}</td>
-
-                    <td>
-                      <button onClick={toggleShow} className="btn btn-success">
-                        Edit
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-
-          <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
-            <MDBModalDialog>
-              <MDBModalContent>
-                <MDBModalHeader>
-                  <MDBModalTitle>Modal title</MDBModalTitle>
-                  <MDBBtn
-                    className="btn-close"
-                    color="none"
-                    onClick={toggleShow}
-                  ></MDBBtn>
-                </MDBModalHeader>
-                <MDBModalBody>
-                  <button>Admin</button>
-                  <button>User</button>
-                </MDBModalBody>
-
-                <MDBModalFooter>
-                  <MDBBtn color="secondary" onClick={toggleShow}>
-                    Close
-                  </MDBBtn>
-                  <MDBBtn>Save changes</MDBBtn>
-                </MDBModalFooter>
-              </MDBModalContent>
-            </MDBModalDialog>
-          </MDBModal>
-        </div>
-      </div>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default EmpListing;
+export default Users;
