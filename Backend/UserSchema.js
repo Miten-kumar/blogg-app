34123f8b2d5b2d757127837f02bbab1b445ff1df
mitenAspire <mitenkumar.patel@aspiresoftserv.in>
2023-03-28 19:31:39 +0530

    "Stable"

--- Backend/UserSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  id:Number,
-  email: String,
-  role: String,
-});
-module.exports= mongoose.model("users",userSchema)
